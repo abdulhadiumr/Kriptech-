c9027cbe9d5dccd4260c5a127cc5de6013a7b8e0
abdulhadiumr <abdulhadiumar3346@gmail.com>
2025-05-11 16:21:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ FAUCETPAY_API_KEY = "eb70ce2715bea1e955fb023dda9cf9ac17b1aa759d6c1c86f4dab36495d
 FAUCETPAY_WITHDRAW_URL = "https://faucetpay.io/api/v1/withdraw"  # Confirm exact URL from docs
 REWARD_AMOUNT = 0.0001  # Initial bonus and reward amount in TON
 MIN_WITHDRAWAL = 0.00001  # Minimum withdrawal amount
+LOG_LEVEL = "INFO"

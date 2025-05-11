ab0e7f38883bdbd815647a55de4b395ae335c5e8
abdulhadiumr <abdulhadiumar3346@gmail.com>
2025-05-11 16:15:42 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
 # config.py
-TELEGRAM_BOT_TOKEN = "7860323880:AAFVzR9ndFoxXW6JbWX-Mt4RTokLFe9o0_0"  # Replace with your BotFather token
-FAUCETPAY_API_KEY = "your_faucetpay_api_key"    # Replace with your FaucetPay API key
+TELEGRAM_BOT_TOKEN = "8143999351:AAHtIZ4lQvcvSQasM4dAQUPJJ4okL8t3DTg"  # Replace with your BotFather token
+FAUCETPAY_API_KEY = "eb70ce2715bea1e955fb023dda9cf9ac17b1aa759d6c1c86f4dab36495d513d8"    # Replace with your FaucetPay API key
 FAUCETPAY_WITHDRAW_URL = "https://faucetpay.io/api/v1/withdraw"  # Confirm exact URL from docs
 REWARD_AMOUNT = 0.0001  # Initial bonus and reward amount in TON
 MIN_WITHDRAWAL = 0.00001  # Minimum withdrawal amount
-
-# Channel links
-CHANNELS = [
-    {"name": "Telegram Channel", "url": "https://t.me/Nextpipofficial1"},
-    {"name": "YouTube Channel", "url": "https://youtube.com/@nextpip?si=yoHk9Rhe0M03jdDk"},
-    {"name": "Twitter", "url": "https://x.com/crypto_bit62141?t=sxIowiP8g6o7YiQC1XLSoA&s=09"}
-]
